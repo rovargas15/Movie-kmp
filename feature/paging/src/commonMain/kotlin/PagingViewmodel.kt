19079b48420363e1131c42ad29eb186455778ad5
exrvarc <rafael.vargas@externos.bci.cl>
2024-05-28 23:44:51 -0500

    implement paging

--- feature/paging/src/commonMain/kotlin/PagingViewmodel.kt
+++ feature/paging/src/commonMain/kotlin/PagingViewmodel.kt
@@ -8,6 +8,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.flowOn
 import model.Movie
 import usecase.GetPagingMovieByCategory
 
@@ -36,7 +37,7 @@ class PagingViewmodel(
                         }
                     }
                 },
-            ).flow
+            ).flow.flowOn(coroutineDispatcher)
     }
 
     fun handleAction(action: PagingAction) {
